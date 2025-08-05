796811978f26b2a9baf8336ff6d79184055ca57a
Parichu <orgaaume7741@gmail.com>
2025-08-06 00:16:21 +0700

    Basic Link List

--- Python/test_app.py
+++ Python/test_app.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 
+# Basic Link list
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
+class SlinkedList:
+    def __init__(self):
+        self.head = None
 
+    def listprint(self):
+        current = self.head
+        while current:
+            print(current.value, end=' ')
+            current = current.next
+        print()
+
+list = SlinkedList()
+list.head = Node(1)
+l2 = Node(2)
+l3 = Node(3)
+
+#Link the nodes
+list.head.next = l2
+
+#Link the next node
+l2.next = l3
+
+list.listprint()  # Output: 1 2 3
